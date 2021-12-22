1d9ecc1672eff8c12aec221456d06a5c6615a8b4
Hnoob <73231678+noobprogammier@users.noreply.github.com>
2021-12-23 01:11:36 +0200

    Add files via upload

--- lib_instance.py
+++ lib_instance.py
@@ -1,4 +1,5 @@
 from hashy import attribs
+from hashy import * 
 class IntError(Exception):
 	pass 
 """
@@ -22,6 +23,10 @@ class invoke__(object):
 			raise IntError("Required type of 'string' or 'bool' not '%s'!"%(type(kwargs["password"])))
 		elif "limit" in kwargs and type(kwargs["limit"]) != int:
 			raise IntError("Required type of 'int' not '%s'!"%(type(kwargs["limit"])))
+		elif "cipher_keys" in kwargs and type(kwargs["cipher_keys"]) != str:
+			raise IntError("Required type of 'string' not '%s'!"%(type(kwargs["cipher_keys"])))
+		elif "ciphers" in kwargs and type(kwargs["ciphers"]) != str:
+			raise IntError("Required type of 'string' or a 'bool' not '%s'!"%(type(kwargs["ciphers"])))
 		args = {}
 		for items in kwargs:
 			args[items] = kwargs[items]
@@ -39,15 +44,76 @@ class invoke__(object):
 			args["cbc"] = None
 		if "gcm" not in args:
 			args["gcm"] = None
-		if "hashsum" in args:
+		if "ciphers" not in args:
+			args["ciphers"] = None
+		if "cipher_keys" not in args:
+			args["cipher_keys"] = None
+		if "hashsum" in args and args["hashsum"] != None:
 			if type(args["hashsum"]) == list and len(args["hashsum"][0]) != len(attribs.return_sample(args["algorithm"])):
 				raise IntError("Provided string doesn't meet %s requirements."%(args["algorithm"].upper()))
 			else:
 				if len(attribs.return_sample(args["algorithm"])) != len(args["hashsum"]):
 					raise IntError("Provided string doesn't meet MD5 requirements.")
 			self.file_ = attribs.get_words(limit=args["limit"], origin=args["automatew"], depth=args["downloaddepth"])
-			if type(args["hashsum"]) == list or type(args["hashsum"]) == tuple:
-				for hashes in args["hashsum"]:
-					attribs(passwords=self.file_, hashsum=hashes, algorithm=args["algorithm"], view=None, wr=args["automatew"])
-			attribs(passwords=self.file_, hashsum=args["hashsum"], algorithm=args["algorithm"], view=None, wr=args["automatew"])
+			self.mode = "hash_s"
+			self.sum = args["hashsum"]
+			self.list = self.file_
+			self.auto = args["automatew"]
+			self.algo = args["algorithm"]
+		elif "cbc" in args and args["cbc"] != None:
+			if "cipher_keys" not in args:
+				raise NameError("Cannot run the instance, without having defined cipher_keys or vice versa. ")
+			self.file_ = attribs.get_words(limit=args["limit"], origin=args["automatew"], depth=args["downloaddepth"])
+			self.mode = "cbc"
+			self.sum = args["cbc"]
+			self.list = self.file_
+			self.cipher_keys = args["cipher_keys"]
+		elif "gcm" in args and args["gcm"] != None:
+			if "cipher_keys" not in args:
+				raise NameError("Cannot run the instance, without having defined cipher_keys or vice versa. ")
+			self.file_ = attribs.get_words(limit=args["limit"], origin=args["automatew"], depth=args["downloaddepth"])
+			self.mode = "gcm"
+			self.sum = args["gcm"]
+			self.list = self.file_
+			self.cipher_keys = args["cipher_keys"]
+		elif "ciphers" in args and args["ciphers"] != None:
+			if "cipher_keys" not in args or "algorithm" not in args:
+				raise NameError("Cannot run the instance, without having defined cipher_keys and algorithm or vice versa.")
+			self.file_ = attribs.get_words(limit=args["limit"], origin=args["automatew"], depth=args["downloaddepth"])
+			self.mode = "ciphers"
+			self.sum = args["ciphers"]
+			self.enmsg = args["hashsum"]
+			self.list = self.file_
+			self.cipher_keys = args["cipher_keys"]
+	def launch_crack(self, **kwargs):
+		if self.mode == "cbc":
+			if type(self.sum) == list or type(self.sum) == tuple:
+				for sums in self.sum:
+					output_ = StartCBC(list=self.file_, sum=sums, cipher_keys=self.cipher_keys)
+			else:
+				output_ = StartCBC(list=self.file_, sum=self.sum, cipher_keys=self.cipher_keys)
+		elif self.mode == "gcm":
+			if type(self.sum) == list or type(self.sum) == tuple:
+				for sums in self.sum:
+					output_ = StartGCM(list=self.file_, sum=sums, cipher_keys=self.cipher_keys)
+			else:
+				output_ = StartGCM(list=self.file_, sum=self.sum, cipher_keys=self.cipher_keys)
+		elif self.mode == "hash_s":
+			if type(self.sum) == list or type(self.sum) == tuple:
+				for items in self.sum:
+					attribs(passwords=self.list, hashsum=items, algorithm=self.algo, view=None, wr=self.auto)
+			else:
+				attribs(passwords=self.list, hashsum=self.sum, algorithm=self.algo, view=None, wr=self.auto)
+		elif self.mode == "ciphers":
+			x_ = FormList(target=self.list, list_=self.cipher_keys, times=self.sum.replace("3", ""))
+			if type(self.enmsg) == list or type(self.enmsg) == tuple:
+				for items in self.enmsg.split(","):
+					attribs(passwords=x_, hashsum=items, cipher_keys=self.cipher_keys)
+			else:
+				attribs(passwords=x_, hashsum=self.enmsg, cipher_keys=self.cipher_keys)
+
+
+
+
+
 
